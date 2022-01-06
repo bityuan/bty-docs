1bae0927489f838acf776af71532d6aa21dcaa66
wenglk <viken0408@foxmail.com>
2022-01-06 11:01:06 +0800

    20220106-110105

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,9 +4,11 @@ const { getFilesName} = require('../utils/fileName')
 module.exports = config({
   title: "BitYuan",
   description: "A demo for vuepress-theme-hope",
-  base:'/bty-docs/',
-  // docsDir:'docs',
+  base: '/bty-docs/',
   dest: "./docs",
+  head: [
+    ['link', {rel:'icon',href:'./public/favicon.ico'}]
+  ],
   // head: [
   //   ["script", { src: "https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js" }],
   //   [
